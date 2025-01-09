a18cd891d2ec17afb2341cf8c565a1f5025ea0df
Somyapratap-406 <somyapratapsingh406@gmail.com>
2025-01-09 15:46:26 +0530

    Added new print pattern feature in printpattern.java, improved output messages in helloworld.java

--- JAVA/JAVA/helloworld.java
+++ JAVA/JAVA/helloworld.java
@@ -1,4 +1,4 @@
-package JAVA;
+
 
 public class helloworld {
     public static void main (String args[]) {
--- JAVA/JAVA/printpattern.java
+++ JAVA/JAVA/printpattern.java
@@ -1,4 +1,4 @@
-package JAVA;
+
 
 public class printpattern {
     public static void main (String args[]) {
